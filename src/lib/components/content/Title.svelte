ffabac309b9b0c71ad3c249674b2db3cfa4ea4e4
Powerbored <andrew.daniel.lewis@gmail.com>
2026-03-19 00:00:58 +1100

    improve title gradient colour and clipping

--- src/lib/components/content/Title.svelte
+++ src/lib/components/content/Title.svelte
@@ -4,7 +4,7 @@
 
 <svelte:element
     this={type}
-    class="text-4xl md:text-6xl font-black italic tracking-tighter text-transparent bg-clip-text bg-gradient-to-br from-gray-900 to-gray-600 dark:from-white dark:to-gray-400 drop-shadow-sm"
+    class="text-4xl/[1.5] md:text-6xl/[1.5] font-black italic tracking-tighter text-transparent bg-clip-text bg-gradient-to-br from-gray-900 to-cyan-600 dark:from-white dark:to-cyan-400 drop-shadow-sm"
 >
     {#if type === "h1"}
         <span id="main-title">{text}</span>
